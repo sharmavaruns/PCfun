e3193556f8abee7bae0f8559f838e42ddc457437
sharmavaruns <varunsharma.us@gmail.com>
2020-12-31 03:41:20 -0500

    Adding some print statements into pcfun.download()

--- pcfun/download.py
+++ pcfun/download.py
@@ -5,6 +5,8 @@ from botocore.config import Config
 import os
 import zipfile
 
+print('Should be installing required pcfun data into your home directory then unzipping the hidden folder.')
+
 BUCKET_NAME = 'pcfun-download'
 PATH = '.pcfun.zip'
 home_dir = os.path.expanduser("~")
@@ -12,6 +14,7 @@ home_dir = os.path.expanduser("~")
 s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
 
 try:
+    print("Downloading .pcfun.zip from public S3 bucket.")
     s3.Bucket(BUCKET_NAME).download_file(PATH, os.path.join(home_dir,PATH))
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
@@ -21,5 +24,6 @@ except botocore.exceptions.ClientError as e:
     else:
         raise
 
+print('Extracting zipped folder and storing it in home directory.')
 with zipfile.ZipFile(os.path.join(home_dir,PATH), 'r') as zip_ref:
     zip_ref.extractall(os.path.join(home_dir))
